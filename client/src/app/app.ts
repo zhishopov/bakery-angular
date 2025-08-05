418ab37015be7b29f84a4bda45c235c98b257a6f
Zhivko Shopov <zhishopov@gmail.com>
2025-08-05 23:31:50 +0100

    Add Header component

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './shared/components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('bakery-app');
