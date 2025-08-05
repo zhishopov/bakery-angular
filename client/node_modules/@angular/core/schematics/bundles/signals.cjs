9b59960f9372e216551f6559945ad0282728f3fe
Zhivko Shopov <zhishopov@gmail.com>
2025-08-05 22:35:52 +0100

    Organize folders

---
+++ client/node_modules/@angular/core/schematics/bundles/signals.cjs
@@ -0,0 +1,55 @@
+'use strict';
+/**
+ * @license Angular v20.1.4
+ * (c) 2010-2025 Google LLC. https://angular.io/
+ * License: MIT
+ */
+'use strict';
+
+var schematics = require('@angular-devkit/schematics');
+var signalQueriesMigration = require('./signal-queries-migration.cjs');
+var signalInputMigration = require('./signal-input-migration.cjs');
+var outputMigration = require('./output-migration.cjs');
+require('./checker-B1MkHERe.cjs');
+require('typescript');
+require('os');
+require('fs');
+require('module');
+require('path');
+require('url');
+require('./index-C7alPIzS.cjs');
+require('node:path');
+require('./project_paths-KZ5syi8v.cjs');
+require('@angular-devkit/core');
+require('node:path/posix');
+require('./project_tsconfig_paths-CDVxT6Ov.cjs');
+require('./apply_import_manager-BwG_XNz3.cjs');
+require('./migrate_ts_type_references-BXdun0jT.cjs');
+require('assert');
+require('./index-C4RI5-xz.cjs');
+require('./leading_space-D9nQ8UQC.cjs');
+
+function migrate(options) {
+    // The migrations are independent so we can run them in any order, but we sort them here
+    // alphabetically so we get a consistent execution order in case of issue reports.
+    const migrations = options.migrations.slice().sort();
+    const rules = [];
+    for (const migration of migrations) {
+        switch (migration) {
+            case "inputs" /* SupportedMigrations.inputs */:
+                rules.push(signalInputMigration.migrate(options));
+                break;
+            case "outputs" /* SupportedMigrations.outputs */:
+                rules.push(outputMigration.migrate(options));
+                break;
+            case "queries" /* SupportedMigrations.queries */:
+                rules.push(signalQueriesMigration.migrate(options));
+                break;
+            default:
+                throw new schematics.SchematicsException(`Unsupported migration "${migration}"`);
+        }
+    }
+    return schematics.chain(rules);
+}
+
+exports.migrate = migrate;
