aa7dcf37598058262a5872a5eb548a2432e75109
Zhivko Shopov <zhishopov@gmail.com>
2025-08-06 13:23:53 +0100

    Refactor routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,11 +1,31 @@
 import { Routes } from '@angular/router';
-import { Home } from './features/home/home';
-import { Menu } from './features/menu/menu';
-import { Login } from './features/auth/login/login';
 
 export const routes: Routes = [
-  { path: '', component: Home },
-  { path: 'home', component: Home },
-  { path: 'menu', component: Menu },
-  { path: 'login', component: Login },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./features/home/home').then((c) => c.Home),
+  },
+  {
+    path: 'menu',
+    loadComponent: () => import('./features/menu/menu').then((c) => c.Menu),
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./features/auth/login/login').then((c) => c.Login),
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./features/auth/register/register').then((c) => c.Register),
+  },
+  {
+    path: '**',
+    redirectTo: '/home',
+  },
 ];
