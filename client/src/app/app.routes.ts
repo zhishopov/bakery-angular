7d31587cbaa0f305ab9ed1befcd860cd5313c078
Zhivko Shopov <zhishopov@gmail.com>
2025-08-06 08:34:16 +0100

    Setup basic routing and products model

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
+import { Menu } from './features/menu/menu';
+import { Login } from './features/auth/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'menu', component: Menu },
+  { path: 'login', component: Login },
+];
---
+++ client/src/app/features/menu/menu.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  imports: [],
+  templateUrl: './menu.html',
+  styleUrl: './menu.css'
+})
+export class Menu {
+
+}
