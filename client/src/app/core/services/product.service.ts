cafdb57e43cf83f51730cc9e2c665feb400fc096
Zhivko Shopov <zhishopov@gmail.com>
2025-08-06 09:07:07 +0100

    Implement menu page

---
+++ client/src/app/core/services/product.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product } from '../../models/product';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  private readonly apiUrl = 'http://localhost:3030/products';
+
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.apiUrl);
+  }
+}
--- client/src/app/features/menu/menu.ts
+++ client/src/app/features/menu/menu.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal, effect } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductService } from '../../core/services/product.service';
+import { Product } from '../../models/product';
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './menu.html',
-  styleUrl: './menu.css'
+  styleUrl: './menu.css',
 })
 export class Menu {
+  private readonly productService = inject(ProductService);
 
+  readonly products = signal<Product[]>([]);
+
+  constructor() {
+    effect(() => {
+      this.productService.getAll().subscribe((data) => this.products.set(data));
+    });
+  }
 }
