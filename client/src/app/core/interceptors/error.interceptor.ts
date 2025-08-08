4cade5ea50da3a5cb45e4d1241f5f41a2ec3a00b
Zhivko Shopov <zhishopov@gmail.com>
2025-08-08 19:56:45 +0100

    Refactor error interceptor

--- client/src/app/core/interceptors/error.interceptor.ts
+++ client/src/app/core/interceptors/error.interceptor.ts
@@ -1,26 +1,38 @@
 import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { ErrorService } from '../services/error.service';
-import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
 import { catchError, throwError } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { ErrorService } from '../services/error.service';
 
 export const ErrorInterceptor: HttpInterceptorFn = (req, next) => {
-  const errorService = inject(ErrorService);
   const authService = inject(AuthService);
   const router = inject(Router);
+  const errorService = inject(ErrorService);
 
   return next(req).pipe(
     catchError((error: HttpErrorResponse) => {
-      const message =
-        error?.error?.message || error.message || 'Request failed';
-      errorService.setError(message);
+      const status = error.status;
 
-      const unauthorized = error.status === 401 || error.status === 403;
-      const invalidToken = /invalid access token/i.test(message);
-      if (unauthorized && invalidToken) {
-        authService.forceLogout();
-        router.navigate(['/auth/login']);
+      if ((status === 401 || status === 403) && authService.isLoggedIn) {
+        const msg = 'Your session has expired. Please log in again.';
+        errorService.setError(msg);
+
+        authService.logout().subscribe({
+          error: () => {
+            router.navigate(['/auth/login'], { state: { message: msg } });
+          },
+          complete: () => {
+            router.navigate(['/auth/login'], { state: { message: msg } });
+          },
+        });
+      } else {
+        const msg =
+          error?.error?.message ||
+          error?.error?.error?.message ||
+          error.message ||
+          'Request failed.';
+        errorService.setError(msg);
       }
 
       return throwError(() => error);
