b63f28ae3ac590290f538cb097dcd4f9f130ddb9
Zhivko Shopov <zhishopov@gmail.com>
2025-08-08 12:58:45 +0100

    Update product service so Admin can manage products

--- client/src/app/core/services/product.service.ts
+++ client/src/app/core/services/product.service.ts
@@ -1,21 +1,46 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Product } from '../../models/product';
 import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
-@Injectable({
-  providedIn: 'root',
-})
+export interface ProductCreate {
+  name: string;
+  price: number;
+  description: string;
+  image: string;
+  likes?: number;
+  bestSeller?: boolean;
+}
+
+@Injectable({ providedIn: 'root' })
 export class ProductService {
+  private readonly http = inject(HttpClient);
+  private readonly auth = inject(AuthService);
   private readonly apiUrl = 'http://localhost:3030/data/products';
 
-  constructor(private http: HttpClient) {}
-
-  getAll(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.apiUrl);
+  getAll(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
   }
 
-  getById(id: string): Observable<Product> {
-    return this.http.get<Product>(`${this.apiUrl}/${id}`);
+  getById(id: string): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/${id}`);
+  }
+
+  create(product: ProductCreate): Observable<any> {
+    return this.http.post<any>(this.apiUrl, product, {
+      headers: this.auth.getAuthHeaders(),
+    });
+  }
+
+  update(id: string, changes: Partial<ProductCreate>): Observable<any> {
+    return this.http.patch<any>(`${this.apiUrl}/${id}`, changes, {
+      headers: this.auth.getAuthHeaders(),
+    });
+  }
+
+  delete(id: string): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`, {
+      headers: this.auth.getAuthHeaders(),
+    });
   }
 }
