d9dc5511f32cf6d35e31e376ef214b5aa9bbde03
Zhivko Shopov <zhishopov@gmail.com>
2025-08-08 15:05:07 +0100

    Admin can now remove products from menu

--- client/src/app/features/menu/menu.ts
+++ client/src/app/features/menu/menu.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ProductService } from '../../core/services/product.service';
 import { Product } from '../../models/product';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-menu',
@@ -12,12 +13,29 @@ import { Product } from '../../models/product';
 })
 export class Menu {
   private readonly productService = inject(ProductService);
+  private readonly authService = inject(AuthService);
 
   readonly products = signal<Product[]>([]);
 
   constructor() {
     effect(() => {
       this.productService.getAll().subscribe((data) => this.products.set(data));
+    });
+  }
+
+  get isAdmin(): boolean {
+    return this.authService.isAdmin;
+  }
+
+  removeProduct(id: string) {
+    if (!this.isAdmin) return;
+    this.productService.delete(id).subscribe({
+      next: () => {
+        this.products.set(this.products().filter((p) => p._id !== id));
+      },
+      error: (err) => {
+        console.warn('Failed to delete product:', err);
+      },
     });
   }
 }
