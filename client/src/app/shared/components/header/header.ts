c137ce423000c3f5ba37393ebd280d6cbf1a2ec2
Zhivko Shopov <zhishopov@gmail.com>
2025-08-11 12:33:17 +0100

    Add header styles

--- client/src/app/shared/components/header/header.ts
+++ client/src/app/shared/components/header/header.ts
@@ -1,26 +1,26 @@
 import { Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.html',
   styleUrl: './header.css',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
 })
 export class Header {
   private readonly authService = inject(AuthService);
 
-  get isLoggedIn(): boolean {
+  get isLoggedIn() {
     return this.authService.isLoggedIn;
   }
 
-  get isAdmin(): boolean {
+  get isAdmin() {
     return this.authService.isAdmin;
   }
 
-  get userEmail(): string {
-    return this.authService.currentUser?.email ?? '';
+  get userEmail(): string | null {
+    return this.authService.currentUser?.email ?? null;
   }
 
   logout(): void {
